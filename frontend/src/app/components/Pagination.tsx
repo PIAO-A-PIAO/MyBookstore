55818372f16539c1f162a1af489872b42ab93ff3
PIAO-A-PIAO <piaoxuanyi2001@163.com>
2024-04-15 00:40:10 -0400

    update pagination

--- frontend/src/app/components/Pagination.tsx
+++ frontend/src/app/components/Pagination.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAppSelector } from "../redux/store";
+import { selectMobileView } from "../redux/popupSlice";
 
 interface PaginationProps {
   currentPage: number;
@@ -14,34 +16,39 @@ const Pagination: React.FC<PaginationProps> = ({
   const handleClick = (pageNumber: number) => {
     onPageChange(pageNumber);
   };
+  const mobileView = useAppSelector(selectMobileView);
 
   const getPageNumbers = () => {
-    const maxPageNumbers = 9; // Maximum number of visible page numbers
-    const pageNumbers: number[] = [];
+    const maxPageNumbers = mobileView ? 5 : 9; // Maximum number of visible page numbers
+    const pageNumbers = [];
 
     if (totalPages <= maxPageNumbers) {
-      // Less than or equal to 9 pages, display all page numbers
+      // Less than or equal to maxPageNumbers pages, display all page numbers
       for (let i = 1; i <= totalPages; i++) {
         pageNumbers.push(i);
       }
     } else {
-      // More than 9 pages
-      if (currentPage <= 4) {
+      // More than maxPageNumbers pages
+      if (currentPage <= Math.floor(maxPageNumbers / 2) + 1) {
         // Current page is close to the beginning
-        for (let i = 1; i <= 8; i++) {
+        for (let i = 1; i <= maxPageNumbers - 1; i++) {
           pageNumbers.push(i);
         }
         pageNumbers.push(totalPages);
-      } else if (currentPage >= totalPages - 4) {
+      } else if (currentPage >= totalPages - Math.floor(maxPageNumbers / 2)) {
         // Current page is close to the end
         pageNumbers.push(1);
-        for (let i = totalPages - 7; i <= totalPages; i++) {
+        for (let i = totalPages - maxPageNumbers + 2; i <= totalPages; i++) {
           pageNumbers.push(i);
         }
       } else {
         // Current page is in the middle
         pageNumbers.push(1);
-        for (let i = currentPage - 3; i <= currentPage + 3; i++) {
+        for (
+          let i = currentPage - Math.floor(maxPageNumbers / 2);
+          i <= currentPage + Math.floor(maxPageNumbers / 2) - 2;
+          i++
+        ) {
           pageNumbers.push(i);
         }
         pageNumbers.push(totalPages);
@@ -51,6 +58,12 @@ const Pagination: React.FC<PaginationProps> = ({
     return pageNumbers;
   };
 
+  const [pageNumbers, setPageNumbers] = useState(getPageNumbers());
+
+  useEffect(() => {
+    setPageNumbers(getPageNumbers());
+  }, [mobileView, currentPage]);
+
   return (
     <div className="flex justify-center py-4">
       <button
@@ -58,7 +71,7 @@ const Pagination: React.FC<PaginationProps> = ({
         onClick={() => handleClick(currentPage - 1)}
       >
         <svg
-          className="w-6 h-6 text-gray-600"
+          className={`${mobileView ? "w-4 h-4" : "w-6 h-6"} text-gray-600`}
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -75,10 +88,12 @@ const Pagination: React.FC<PaginationProps> = ({
           />
         </svg>
       </button>
-      {getPageNumbers().map((pageNumber) => (
+      {pageNumbers.map((pageNumber) => (
         <button
           key={pageNumber}
-          className={`mx-2 px-3 py-1 ${
+          className={` py-1 ${
+            mobileView ? "text-body4 mx-1 px-2" : "text-body2 mx-2 px-3"
+          } ${
             pageNumber === currentPage
               ? "text-black border-b border-black"
               : "text-gray-600"
@@ -93,7 +108,7 @@ const Pagination: React.FC<PaginationProps> = ({
         onClick={() => handleClick(currentPage + 1)}
       >
         <svg
-          className="w-6 h-6 text-gray-600"
+          className={`${mobileView ? "w-4 h-4" : "w-6 h-6"} text-gray-600`}
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
